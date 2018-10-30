0ef4bcddf0a93cb5003b7ec44d34b2e9bafa957a
dongshaoxiao <shaoxiao.dong@rogrand.com>
2018-10-30 11:04:05 +0800

    微信扫码支付

---
+++ pay/HttpUtil.java
@@ -0,0 +1,66 @@
+package com.rogrand.enterprisehome.modules.pay;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Logger;
+
+/**
+ * 2018/10/30
+ * @author shaoxiao.dong
+ *
+ */
+public class HttpUtil {
+	private final static int CONNECT_TIMEOUT = 5000; // in milliseconds  连接超时的时间  
+    private final static String DEFAULT_ENCODING = "UTF-8";  //字符串编码  
+
+    public static String postData(String urlStr, String data){    
+        return postData(urlStr, data, null);    
+    }    
+    /** 
+     * post数据请求 
+     * @param urlStr 
+     * @param data 
+     * @param contentType 
+     * @return 
+     */  
+    public static String postData(String urlStr, String data, String contentType){    
+        BufferedReader reader = null;    
+        try {    
+            URL url = new URL(urlStr);    
+            URLConnection conn = url.openConnection();    
+            conn.setDoOutput(true);    
+            conn.setConnectTimeout(CONNECT_TIMEOUT);    
+            conn.setReadTimeout(CONNECT_TIMEOUT);    
+            if(contentType != null)    
+                conn.setRequestProperty("content-type", contentType);    
+            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream(), DEFAULT_ENCODING);    
+            if(data == null)    
+                data = "";    
+            writer.write(data);     
+            writer.flush();    
+            writer.close();      
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), DEFAULT_ENCODING));    
+            StringBuilder sb = new StringBuilder();    
+            String line = null;    
+            while ((line = reader.readLine()) != null) {    
+                sb.append(line);    
+                sb.append("\r\n");    
+            }    
+            return sb.toString();    
+        } catch (IOException e) {    
+        System.out.println("Error connecting to " + urlStr + ": " + e.getMessage());
+        } finally {    
+            try {    
+                if (reader != null)    
+                    reader.close();    
+            } catch (IOException e) {    
+            }    
+        }    
+        return null;    
+    }    
+}
---
+++ pay/WeChatConfig.java
@@ -0,0 +1,52 @@
+package com.rogrand.enterprisehome.modules.pay;
+/** 
+ * 微信支付配置文件 
+ * 2018/10/30
+ * @author shaoxiao.dong 
+ * 
+ */  
+public class WeChatConfig {
+
+	 /**
+     * 微信开发平台应用ID
+     */
+    public static final String APP_ID="wx0a3242424aae13c6";
+    /**
+     * 应用对应的凭证
+     */
+    public static final String APP_SECRET="1d29d733ewrewrfwed1b58968cce";
+    /**
+     * 应用对应的密钥
+     */
+    public static final String APP_KEY="dfsfdvdfvgk32423423oGdfsfdsvBO66";
+    /**
+     * 微信支付商户号
+     */
+    public static final String MCH_ID="1469dfsf02";
+    /**
+     * 商品描述
+     */
+    public static final String BODY="QQ游戏-账户充值";
+    /**
+     * 商户号对应的密钥
+     */
+    public static final String PARTNER_key="123466";
+    
+    /**
+     * 商户id
+     */
+    public static final String PARTNER_ID="14698sdfs402dsfdew402";
+    /**
+     * 常量固定值
+     */
+    public static final String GRANT_TYPE="client_credential";
+    /**
+     * 获取预支付id的接口url
+     */
+    public static String UFDODER_URL = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+    /**
+     * 微信服务器回调通知url
+     */
+    public static String WECHAT_NOTIFY_URL_PC="http://www.bairuoheng.cn/app/tenpay/notify";
+
+}
